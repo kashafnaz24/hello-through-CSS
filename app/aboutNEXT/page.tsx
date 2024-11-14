a66b53c3d8e8b33669a5d2603c2b8528924ac185
kashafnaz24 <kashafnazk389@gmail.com>
2024-11-14 15:00:38 +0500

    hello to next through custom css

---
+++ app/aboutNEXT/page.tsx
@@ -0,0 +1,36 @@
+
+
+import Image from "next/image"
+ export default function aboutNEXT(){
+    return(
+        <div id="about">
+            <div id="about_text">
+                <h1>What&apos;s is Next.js?</h1>
+                 <p id="next_mean">Everything you need to build great products on the web.</p>
+                 <p>Next.js is a React framework for building full-stack
+                    web applications. You use React Componentsto build user interfaces, and Next.js for additional
+                    features and optimizations.
+
+                    Under the hood, Next.js also abstracts and automatically configures tooling
+                    needed for React, like bundling, compiling, and more. This allows you to 
+                    focus on building your application instead of spending time with configuration.
+
+                    Whether you&apos;re an individual developer or part of a larger team, Next.js can
+                    help you build interactive, dynamic, and fast React applications.
+                 </p>
+
+            </div>
+            <div id="image">
+        <Image
+        src="/next.logo.png" 
+        alt="image" 
+        width={1800}
+        height={800}
+        priority 
+        />
+        
+            </div>
+        </div>
+    )
+
+}
---
+++ app/portfolio/page.tsx
@@ -0,0 +1,31 @@
+
+import Image from "next/image"
+export default function portfolio(){
+    return(
+        <div id="portfolio">
+            <div id="portfolio_text">
+                <h1>our journey with cashing</h1>
+                <p>Frontend performance can be hard to get right. Even in highly optimized apps, the most common 
+                    culprit by far is client-server waterfalls. When introducing Next.js App,
+                    we knew we wanted to solve this issue. To do that, we needed to 
+                    move client-server REST fetches to the server using React Server Components 
+                    in a single roundtrip. This meant the server had to sometimes be dynamic,
+                    sacrificing the great initial loading performance of Jamstack. We built
+                    partial prerendering to solve this tradeoff and have the best of both worlds.
+
+                </p>
+            </div>
+            <div id="image">
+            <Image
+            src="/next.logo.png" 
+            alt="image" 
+            width={1800}
+            height={800}
+             priority 
+            />
+        
+            
+            </div>
+        </div>
+    )
+}
